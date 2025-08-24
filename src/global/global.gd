f7a229e3dc130e5ab3612193d087edb4ef69604c
Alexander Olferuk <olferukab@gmail.com>
2025-08-24 12:52:24 +0300

    Bones

---
+++ src/global/global.gd
@@ -0,0 +1,6 @@
+extends Node
+
+# var main: Main Scene
+var rng: RandomNumberGenerator
+# var managers: ....
+var gameSpeed: float = 1.0
---
+++ src/global/signal_bus.gd
@@ -0,0 +1,4 @@
+extends Node
+
+# all game-wide signals like score_updated or whatever go here
+#signal achievedSomething(data: float)
