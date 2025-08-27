103ba506b1669b5347fe459931160deffb4d5f1b
Oles <gedzoles@gmail.com>
2025-08-27 22:19:58 +0300

    undo + command

---
+++ src/commands/command.gd
@@ -0,0 +1,8 @@
+class_name Command
+extends RefCounted
+
+
+func execute(hound: Hound) -> bool:
+	return true
+
+func undo(hound: Hound) -> void: pass
---
+++ src/commands/move_command.gd
@@ -0,0 +1,21 @@
+extends Command
+class_name MoveCommand
+
+var direction: Vector2i
+var prev_head_coords: Vector2i
+var prev_direction: Vector2i
+var prev_occupied: Array[Vector2i]
+var prev_sections: Array[Sprite2D]
+
+func _init(dir: Vector2i) -> void:
+	direction = dir
+
+func execute(hound: Hound) -> bool:
+	prev_head_coords = hound.head_coords
+	prev_direction = hound.prev_direction
+	prev_occupied = hound.occupied_cells.duplicate(true)
+	prev_sections = hound._clone_sections()
+	return hound._execute_move(direction)
+
+func undo(hound: Hound) -> void:
+	hound._restore_state(prev_head_coords, prev_direction, prev_occupied, prev_sections)
