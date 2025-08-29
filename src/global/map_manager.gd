6c9f5e972456f526ff40fa5fb3d2b23348531817
Alexander Olferuk <olferukab@gmail.com>
2025-08-29 16:04:40 +0300

    Map manager method fix

--- src/global/map_manager.gd
+++ src/global/map_manager.gd
@@ -17,13 +17,17 @@ func register(entity: LevelEntity, cell: Vector2i) -> void:
 
 func is_walkable(cell: Vector2i) -> bool:
 	var tile_data: TileData = map.get_cell_tile_data(_real_cell(cell))
-	var walkable: bool = tile_data.get_custom_data("walkable") as bool
-	if not walkable:
-		return walkable
-	var obj = level_objects.get(_real_cell(cell))
-	if obj == null:
-		return true
-	return (obj as LevelEntity).pushable
+	if tile_data == null:
+		push_warning("Sky tile taken probably; check dog_head_shift value! Got cell ", cell, " and head shift of ", dog_head_shift)
+		return false
+	else:
+		var walkable: bool = tile_data.get_custom_data("walkable") as bool
+		if not walkable:
+			return walkable
+		var obj = level_objects.get(_real_cell(cell))
+		if obj == null:
+			return true
+		return (obj as LevelEntity).pushable
 
 func visit(cell: Vector2i):
 	SignalBus.cell_visited.emit(_real_cell(cell))
