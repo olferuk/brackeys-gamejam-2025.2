0a8906cf73371c83e6ddc7428994028ca3f2f70a
Oles <gedzoles@gmail.com>
2025-08-30 22:17:18 +0300

    fix order

--- src/objects/spikes.gd
+++ src/objects/spikes.gd
@@ -11,11 +11,11 @@ func _ready() -> void:
 	super._ready()
 
 func player_moved() -> void:
-	if MapManager.is_occupied_by_dog(cell) and !_hidden:
-		SignalBus.emit_signal("level_lost")
 	if _moving:
 		_hidden = !_hidden
 	if _hidden:
 		sprite.texture = textures[0]
 	else:
 		sprite.texture = textures[1]
+	if MapManager.is_occupied_by_dog(cell) and !_hidden:
+		SignalBus.emit_signal("level_lost")
