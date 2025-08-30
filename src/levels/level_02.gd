d4bb379a22e347b0c91769d7fd7bcbed8121b733
Alexander Olferuk <olferukab@gmail.com>
2025-08-30 22:21:11 +0300

    Better context help

--- src/levels/level_02.gd
+++ src/levels/level_02.gd
@@ -6,7 +6,18 @@ func _ready() -> void:
 	MapManager.level = 2
 	MapManager.dog_head_shift = Vector2i(0, 2)
 	
+	SignalBus.cell_visited.connect(Callable(self, "route"))
 	SignalBus.level_won.connect(Callable(self, "onto_next_level"))
+
+func route(cell: Vector2i):
+	if cell in [
+		Vector2i(8, 4),
+		Vector2i(9, 4),
+		Vector2i(9, 5),
+	]:
+		$UI/ContextHelpShrink.visible = true
+	elif cell == Vector2i(7, 5):
+		$UI/ContextHelpShrink.visible = false
 
 func onto_next_level():
 	Transition.load_scene("res://scenes/levels/level03.tscn")
